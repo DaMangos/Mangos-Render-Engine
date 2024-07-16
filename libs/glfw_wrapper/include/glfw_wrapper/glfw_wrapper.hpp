3d9a35eb55321188c4f0a2caa99650001423bc7a
DaMangos <oliverhorriganpierre@outlook.com>
2024-07-16 22:20:01 +0100

    added spv support

--- libs/glfw_wrapper/include/glfw_wrapper/glfw_wrapper.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <glfw_wrapper/version_3_4/handles/cursor.hpp>
-#include <glfw_wrapper/version_3_4/handles/library.hpp>
-#include <glfw_wrapper/version_3_4/handles/monitor.hpp>
-#include <glfw_wrapper/version_3_4/handles/nullhandle.hpp>
-#include <glfw_wrapper/version_3_4/handles/window.hpp>
--- libs/vulkan_wrapper/include/vulkan_wrapper/vulkan_wrapper.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <vulkan_wrapper/version_1_0/device.hpp>
-#include <vulkan_wrapper/version_1_0/instance.hpp>
-#include <vulkan_wrapper/version_1_0/nullhandle.hpp>
-#include <vulkan_wrapper/version_1_0/physical_device.hpp>
-#include <vulkan_wrapper/version_1_0/range_element_handle.hpp>
-#include <vulkan_wrapper/version_1_0/shared_handle.hpp>
-#include <vulkan_wrapper/version_1_0/unique_handle.hpp>
